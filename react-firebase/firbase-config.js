cb16f4b20ecbf4dfb5ed140a791415dfc043fb2f
Chawput <chawput@gmail.com>
2021-11-04 14:45:09 +0700

    add firebase-config.js

---
+++ react-firebase/firbase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXWmC9x6X_sxxxxxxxxxx",
+  authDomain: "react-http-xxxxxxxx.firebaseapp.com",
+  databaseURL: "https://react-http-xxxxxxx.asia-southeast1.firebasedatabase.app",
+  projectId: "react-http-xxxxxxxxx",
+  storageBucket: "react-http-xxxxxxxx.appspot.com",
+  messagingSenderId: "xxxxxxxxxxxxxx",
+  appId: "1:xxxxxxx:web:xxxxxxxxxxxx"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
